6fd25037af2276a02075d8d3221679b1d6311e77
syd enciso <encisosydney19@gmail.com>
2024-10-29 12:59:06 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import routes from "./Routes";
 import { MongoDB } from "./Utils/dbConnect";
+import { Get } from "./Handlers/blogPost";
 
 declare global {
   namespace Express {
@@ -24,6 +25,8 @@ application.use(routes);
 application.get("/", (request: any, response: any) => {
   response.status(200).json({ message: "Hello Api" });
 });
+
+application.get("/api/v2/blogpost", Get.getAll);
 
 application.listen(process.env.PORT, () => {
   console.log(`Development server is running on port ${process.env.PORT}`);
